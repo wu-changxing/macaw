c7dbfa4386c8717352dd349552c378d5eba7381d
aaron <xietianle@outlook.com>
2022-08-30 22:39:34 +1000

    get key from environment

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,7 +1,13 @@
 from .base import *
+import os
+
 
 DEBUG = False
 ALLOWED_HOSTS = ["aaron404.com"]
+try:
+    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+except:
+    print('cant import secret')
 try:
     from .local import *
 except ImportError:
