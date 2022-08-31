623970b6e2eef7125512c73391e8bc0684f43bcc
aaron <xietianle@outlook.com>
2022-08-31 22:43:59 +1000

    add used time

--- blog/gcv/wcover.py
+++ blog/gcv/wcover.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import jieba
 import jieba.analyse
-
+from time import time
 from string import punctuation
-import asyncio
+
 
 def clean_tags(word:str):
     '''
@@ -23,6 +23,7 @@ def clean_tags(word:str):
 
 def generate_img(article:str,path:str):
     print('generating....')
+
     article = clean_tags(article)
     print('cleaned')
     jieba.analyse.set_stop_words('./blog/gcv/stopwords.txt')
@@ -30,18 +31,21 @@ def generate_img(article:str,path:str):
     keywords = dict()
     for i in result:
       keywords[i[0]]=i[1]
-
+    print('getted keywords')
+    start = time()
     wc = WordCloud(
-        background_color='white',  # 背景颜色，根据图片背景设置，默认为黑色
+        # background_color='white',  # 背景颜色，根据图片背景设置，默认为黑色
         # mask = backgroup_Image, #笼罩图
         font_path='./blog/gcv/fonts/SourceHanSerif/SourceHanSerifK-Light.otf',  # 若有中文需要设置才会显示中文
         width=1920,
         height=1186,
-        margin=2)
-    wc.generate_from_frequencies(keywords)
+        margin=2).generate_from_frequencies(keywords)
     # 参数 width，height，margin分别对应宽度像素，长度像素，边缘空白
     # 保存图片：默认为此代码保存的路
     wc.to_file(path)
+
+    end = time()
+    print('used',end-start)
     print('finished')
 
 if __name__ == '__main__':
