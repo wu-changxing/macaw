032f32670cf2334bf79ee71d34dfc9c85a4edeb6
aaron <xietianle@outlook.com>
2022-08-31 19:49:26 +1000

    add secret key setting

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,10 +1,13 @@
 from .base import *
+from .secret import *
 import os
 
 
 DEBUG = False
 ALLOWED_HOSTS = ["aaron404.com"]
+
 try:
-    SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+    SECRET_KEY  = DJANGO_SECRET_KEY
+    # SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 except:
     print('cant import secret')
