29a335b32fee6b6b2cddb47ab0139550e7e7a395
server1 <server1@vultr.com>
2022-09-06 11:32:30 +0800

    server side

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,12 +1,10 @@
 from .base import *
 from .secret import *
 import os
-
 WAGTAILADMIN_BASE_URL = "https://aaron404.com"
 
 DEBUG = False
 ALLOWED_HOSTS = ["aaron404.com"]
-# ALLOWED_HOSTS = ["*"]
 
 try:
     SECRET_KEY  = DJANGO_SECRET_KEY
