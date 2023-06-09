ac9c7beb416409293195075bb875150805618c82
aaron <xietianle@outlook.com>
2023-06-09 19:10:22 +1000

    local chnage

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,14 +23,14 @@ urlpatterns = [
     path("socket.io/", socket_app),
 ]
 
-# if settings.DEBUG:
-#     from django.conf.urls.static import static
-#     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-#
-#     # Serve static and media files from development server
-#     urlpatterns += staticfiles_urlpatterns()
-#     urlpatterns += static(settings.MEDIA_URL,
-#                           document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+    # Serve static and media files from development server
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
 urlpatterns = urlpatterns + i18n_patterns(
     path("search/", search_views.search, name="search"),
