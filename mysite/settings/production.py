431acc371b1f11178554d546e7fdd3961c190b05
aaron <xietianle@outlook.com>
2023-06-09 19:11:26 +1000

    local chnage

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -5,17 +5,17 @@ from .secret import *
 import os
 WAGTAILADMIN_BASE_URL = "https://aaron404.com"
 # # WAGTAILADMIN_BASE_URL = "0.0.0.0"
-# CSRF_TRUSTED_ORIGINS = ['https://aaron404.com',]
-#
-# DEBUG = True
-# ALLOWED_HOSTS = ["aaron404.com", "localhost", ]
-# CORS_ALLOW_CREDENTIALS = True
-# CORS_ALLOW_ALL_ORIGINS = False
-# CORS_ALLOWED_ORIGINS = [
-#     "https://eac.aaron404.com",
-#     "https://aaron404.com",
-# ]
-#
+CSRF_TRUSTED_ORIGINS = ['https://aaron404.com']
+
+DEBUG = True
+ALLOWED_HOSTS = ["aaron404.com", "localhost" ]
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "https://eac.aaron404.com",
+    "https://aaron404.com",
+]
+
 print('production settings is running')
 try:
     SECRET_KEY = DJANGO_SECRET_KEY
