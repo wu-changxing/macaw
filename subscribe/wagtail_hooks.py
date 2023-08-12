18ffacd657445b41ea576c917f20431a41525af7
aaron <xietianle@outlook.com>
2023-08-12 23:29:48 +1000

    update the subscriber

--- subscribe/wagtail_hooks.py
+++ subscribe/wagtail_hooks.py
@@ -1,17 +1,19 @@
 # subscribe/wagtail_hooks.py
-from wagtail.contrib.modeladmin.options import (
-    ModelAdmin, modeladmin_register
-)
 from .models import Subscriber
 
-class SubscriberAdmin(ModelAdmin):
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.views.snippets import SnippetViewSet
+
+class SubscriberSnippetViewSet(SnippetViewSet):
     model = Subscriber
     menu_label = 'Subscribers'
-    menu_icon = 'pick'
+    icon = 'pick' # Changed from menu_icon
     menu_order = 200
     add_to_settings_menu = False
+    add_to_admin_menu = True  # If you want it on top-level admin menu
     exclude_from_explorer = False
     list_display = ('email', 'created_at')
     search_fields = ('email',)
 
-modeladmin_register(SubscriberAdmin)
+register_snippet(SubscriberSnippetViewSet)
+
