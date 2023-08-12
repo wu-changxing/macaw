3959bfe19279e99f1826fa320af4949fe15be682
aaron <xietianle@outlook.com>
2023-08-12 16:34:06 +1000

    update the urls

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -3,22 +3,30 @@
 from .base import *
 from .secret import *
 import os
+
+# Update the Wagtail admin URL
 WAGTAILADMIN_BASE_URL = "https://backend.aaron404.com"
-# # WAGTAILADMIN_BASE_URL = "0.0.0.0"
+
+# Update trusted origins for CSRF
 CSRF_TRUSTED_ORIGINS = ['https://backend.aaron404.com']
 
 DEBUG = False
+
+# Update allowed hosts
 ALLOWED_HOSTS = ["backend.aaron404.com", "localhost", "127.0.0.1"]
+
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = False
+
+# Update allowed CORS origins
 CORS_ALLOWED_ORIGINS = [
     "https://eac.aaron404.com",
-    "https://aaron404.com",
-    "https://gpt.aaron404.com",
     "https://backend.aaron404.com",
+    "https://gpt.aaron404.com"
 ]
 
 print('production settings is running')
+
 try:
     SECRET_KEY = DJANGO_SECRET_KEY
 except:
