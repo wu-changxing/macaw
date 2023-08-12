d2aecc062f379754defd2949fee182b4ba39ccf0
aaron <xietianle@outlook.com>
2023-08-12 16:13:07 +1000

    update the backend"

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -3,18 +3,19 @@
 from .base import *
 from .secret import *
 import os
-WAGTAILADMIN_BASE_URL = "https://aaron404.com"
+WAGTAILADMIN_BASE_URL = "https://backend.aaron404.com"
 # # WAGTAILADMIN_BASE_URL = "0.0.0.0"
-CSRF_TRUSTED_ORIGINS = ['https://aaron404.com']
+CSRF_TRUSTED_ORIGINS = ['https://backend.aaron404.com']
 
 DEBUG = False
-ALLOWED_HOSTS = ["aaron404.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["backend.aaron404.com", "localhost", "127.0.0.1"]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "https://eac.aaron404.com",
     "https://aaron404.com",
-    "https://gpt.aaron404.com"
+    "https://gpt.aaron404.com",
+    "https://backend.aaron404.com",
 ]
 
 print('production settings is running')
