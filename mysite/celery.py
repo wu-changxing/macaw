6a17e041f4c6042b8b8007522ab810de88e0cf16
aaron <xietianle@outlook.com>
2023-09-29 22:54:04 +1000

    celery

--- mysite/celery.py
+++ mysite/celery.py
@@ -8,11 +8,13 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.production")
 # print(os.environ['DJANGO_SEETINGS_MODULE'])
 app = Celery('mysite')
 
+# TODO: this part need to add the different files to the celery
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
